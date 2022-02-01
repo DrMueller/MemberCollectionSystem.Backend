ef900c0913550286490aba52e6b7d85e2b3a5df1
Matthias Müller <matthias.mueller@trivadis.com>
2022-02-01 10:42:42 +0100

    WIP

---
+++ Sources/Application/Areas/MemberAddresses/Controllers/MemberAddressesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mmu.MemberCollectionSystem.Areas.MemberAddresses.Dtos;
+using System.Diagnostics;
+
+namespace Mmu.MemberCollectionSystem.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MemberAddressesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IReadOnlyCollection<MemberAddressDto>> Get()
+        {
+            Debug.WriteLine("In Get");
+
+            var dtos = Enumerable.Range(1, 5).Select(index => new MemberAddressDto
+            {
+Id = index,
+HouseNumber = 10 + index,
+StreetName = "Street " + index
+            }).ToList();
+
+            return Ok(dtos);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<MemberAddressDto> Get([FromRoute] int id)
+        {
+            Debug.WriteLine("In Get by ID");
+
+            var dto = new MemberAddressDto
+            {
+                Id = id,
+                HouseNumber = 25,
+                StreetName = "Street " + id
+            };
+
+            return Ok(dto);
+        }
+
+        [HttpPut]
+        public ActionResult<MemberAddressDto> Upsert([FromBody] MemberAddressDto dto)
+        {
+            Debug.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(dto));
+
+            return Ok(
+                new MemberAddressDto
+                {
+                    StreetName = dto.StreetName + " Updated",
+                    HouseNumber = dto.HouseNumber + 100,
+                    Id = 50
+                });
+        }
+    }
+}
